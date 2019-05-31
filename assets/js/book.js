e1f35fe060533cdecd3bab45945d6cf05c70a398
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-05-31 18:02:30 +0300

    Initialize project, establish common folder structure, crete book domain

---
+++ assets/js/book.js
@@ -0,0 +1,11 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function() {
+    return "title: " + this.title + " author: " + this.author +
+           " pages: " + this.pages + " read: " + this.read;
+  }
+}
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+alert("Hello, Wordl!");
+let library = [
+  { title: "", author: "", read: true }
+];
+
+var book = new Book("Eloquent Javascript", "Marjin Haverbeke", 450, false);
+console.log(book.info());
