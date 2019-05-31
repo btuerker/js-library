c3a19563ce450f032c4a6e0f4392307cd4271b7e
madcido <fabowow@gmail.com>
2019-05-31 15:52:08 -0300

    dev

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
-alert("Hello, Wordl!");
+// alert("Hello, Wordl!");
 let library = [
   { title: "", author: "", read: true }
 ];
 
-var book = new Book("Eloquent Javascript", "Marjin Haverbeke", 450, false);
-console.log(book.info());
+// var book = new Book("Eloquent Javascript", "Marjin Haverbeke", 450, false);
+// console.log(book.info());
+
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+
+  let book = new Book(title, author, pages, read);
+  library.push(book);
+  alert();
+}
