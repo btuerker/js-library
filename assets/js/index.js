9636132a04d25803363713570a326658cffbd643
madcido <fabowow@gmail.com>
2019-06-04 14:52:14 -0300

    hello ryan

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,93 @@
-let library = setupLibrary();
+const library = setupLibrary();
 
 if (!localStorage.uniqueId) {
-  localStorage.uniqueId = 1; 
+    localStorage.uniqueId = 1;
 }
 
 function setupLibrary() {
-  let library;
-  if(localStorage.library) {
-    library = JSON.parse(localStorage.library).map((e) => Object.assign(new Book(), e));
-  } else {
-    library = [];
-  }
-  return library;
+    if(localStorage.library) {
+        return JSON.parse(localStorage.library).map((book) => Object.assign(new Book(), book));
+    }
+    return [];
 }
 
 function addBookToLibrary() {
-  const form   = document.getElementById("book-form");
+    const title  = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages  = document.getElementById("pages").value;
+    const read   = document.getElementById("read").checked;
 
-  const title  = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages  = document.getElementById("pages").value;
-  const read   = document.getElementById("read").checked;
-
-  const book = new Book(title, author, pages, read);
-  library.push(book);
-  updateLocalStorage("library", library);
-  addBookToShelf(book);
+    const book = new Book(title, author, pages, read);
+    library.push(book);
+    updateLocalStorage("library", library);
+    addBookToShelf(book);
 }
 
 function addBookToShelf(book) {
-  const table = document.getElementById("library-body");
-  const newRow = table.insertRow(table.rows.length);
-  newRow.setAttribute("id", "book-" + book.id);
-  newRow.innerHTML = book.render();
+    const table = document.getElementById("library-body");
+    const newBook = document.createElement("tr");
+    newBook.setAttribute("id", "book-" + book.id);
+    newBook.innerHTML = Book.prototype.render(book);
+    // const newBook = Book.prototype.render(book);
+    table.appendChild(newBook);
 }
 
 function removeBook(id) {
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-    if (result.value) {
-      const book = library.find( (e) => e.id == id );
-      library.splice(library.indexOf(book), 1);
-      updateLocalStorage("library", library);
-      const table = document.getElementById("library-body");
-      table.removeChild(document.getElementById("book-" + book.id));
-      Swal.fire(
-        'Deleted!',
-        'Your book has been deleted.',
-        'success'
-      )
-    }
-  })
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            const book = library.find( (book) => book.id == id );
+            library.splice(library.indexOf(book), 1);
+            updateLocalStorage("library", library);
+            const table = document.getElementById("library-body");
+            table.removeChild(document.getElementById("book-" + book.id));
+            Swal.fire(
+                'Deleted!',
+                'Your book has been deleted.',
+                'success'
+            )
+        }
+    })
 }
 
 function newBookForm() {
-  Swal.fire({
-    title: 'Book Information',
-    html:
-    '<input id="title" class="swal2-input" placeholder="Title">' +
-    '<input id="author" class="swal2-input" placeholder="Author">' +
-    '<input id="pages" class="swal2-input" placeholder="Pages">' +
-    '<input type="checkbox" id="read">' +
-    '<label for="read">&nbsp;&nbsp;Did you read?</label>'
-    ,
-    focusConfirm: false,
-    confirmButtonText: 'Add Book',
-    preConfirm: () => {
-      addBookToLibrary();
-    }
-  });
+    Swal.fire({
+        title: 'Book Information',
+        html:
+        '<input id="title" class="swal2-input" placeholder="Title">' +
+        '<input id="author" class="swal2-input" placeholder="Author">' +
+        '<input id="pages" class="swal2-input" placeholder="Pages">' +
+        '<input type="checkbox" id="read">' +
+        '<label for="read">&nbsp;&nbsp;Did you read?</label>'
+        ,
+        focusConfirm: false,
+        confirmButtonText: 'Add Book',
+        preConfirm: () => {
+            addBookToLibrary();
+        }
+    });
 }
 
 function toggleRead(id) {
-  const book = library.find( (e) => e.id == id );
-  library[library.indexOf(book)].toggleRead();
-  updateLocalStorage("library", library);
+    const index = library.findIndex(book => book.id === id);
+    library[index].toggleRead();
+    updateLocalStorage("library", library);
 }
 
 function updateLocalStorage(key, value) {
-  localStorage[key] = JSON.stringify(value);
+    localStorage[key] = JSON.stringify(value);
 }
 
 function init() {
-  library.forEach((e) => addBookToShelf(e));
+    library.forEach((book) => addBookToShelf(book));
 }
 
 
 init();
-
