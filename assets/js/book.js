34267aa9111cc01a6d1430d0eb14c0128e913db3
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-06-26 00:50:47 +0300

    Fix styling with prettier, add validation to sweetalert form

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,76 +1,36 @@
 function Book(title, author, pages, read) {
-    this.id = parseInt(localStorage.uniqueId);
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.id = parseInt(localStorage.uniqueId);
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 
-    this.toggleRead = function() {
-        this.read = !this.read;
-    }
-
-    localStorage.uniqueId++;
+  localStorage.uniqueId++;
 }
 
-Book.prototype.info = function(book) {
-    return "title: " + book.title + " author: " + book.author +
-    " pages: " + book.pages + " read: " + book.read;
-}
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+};
 
-Book.prototype.render = function(book) {
-      return `
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.pages}</td>
-                <td>
-                 <label class="switch">
-                    <input onchange="toggleRead(${book.id})" type="checkbox" ${(book.read == true) ? 'checked' : null}>
-                    <span class="slider round"></span>
-                  </label>
-                </td>
-                <td><button type="button" onclick="removeBook(${book.id})">Remove</button></td>
-             `
-}
+Book.prototype.info = function() {
+  return `title: ${this.title} author: ${this.author} pages: ${this.pages} read: ${this.read}`;
+};
 
-// Book.prototype.render = function(book) {
-//     let row = document.createElement("tr");
-//     row.setAttribute("id", "book-" + book.id);
-//
-//     let title = document.createElement("td");
-//     title.innerHTML = book.title;
-//
-//     let author = document.createElement("td");
-//     author.innerHTML = book.author;
-//
-//     let pages = document.createElement("td");
-//     pages.innerHTML = book.pages;
-//
-//     let read = document.createElement("td");
-//     let readLabel = document.createElement("label");
-//     readLabel.setAttribute("class", "switch");
-//     let readInput = document.createElement("input");
-//     readInput.setAttribute("type", "checkbox");
-//     readInput.setAttribute("onchange", "toggleRead(" + book.id + ")");
-//     if (book.read) {
-//         readInput.setAttribute("checked", "checked");
-//     }
-//     let readSpan = document.createElement("span");
-//     readSpan.setAttribute("class", "slider round");
-//     readLabel.appendChild(readInput);
-//     readLabel.appendChild(readSpan);
-//     read.appendChild(readLabel);
-//
-//     let del = document.createElement("td");
-//     let delButton = document.createElement("button");
-//     delButton.innerHTML = "Remove";
-//     delButton.setAttribute("type", "button");
-//     delButton.setAttribute("onclick", "removeBook(" + book.id + ")");
-//     del.appendChild(delButton);
-//
-//     row.appendChild(title);
-//     row.appendChild(author);
-//     row.appendChild(pages);
-//     row.appendChild(read);
-//     row.appendChild(del);
-//     return row;
-// }
+Book.prototype.render = function() {
+  return `
+          <td>${this.title}</td>
+          <td>${this.author}</td>
+          <td>${this.pages}</td>
+            <td>
+              <label class="switch">
+                <input onchange="toggleRead(${this.id})" type="checkbox" ${
+    this.read == true ? 'checked' : null
+  }>
+                <span class="slider round"></span>
+              </label>
+            </td>
+          <td><button type="button" onclick="removeBook(${
+            this.id
+          })">Remove</button></td>
+             `;
+};
